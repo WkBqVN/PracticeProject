816ea5d69be027bf1000d4687c3eb186e2df13d5
bsv-khoavo <124114165+bsv-khoavo@users.noreply.github.com>
2023-02-24 13:25:52 +0700

    Update

--- frontend/shophouse/src/App.js
+++ frontend/shophouse/src/App.js
@@ -4,12 +4,14 @@ import  NavbarMain  from './components/NavbarMain';
 import HouseCard from './components/HouseCard';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NotedListHouse from './components/NotedListHouse';
 
 function App() {
   return (
     <div className="App">
       <NavbarMain name = "red"/>
       <HouseCard />
+      <NotedListHouse />
     </div>
   );
 }
---
+++ frontend/shophouse/src/components/NotedListHouse.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import testImg from '../data/dummydata/house1.jpeg';
+//import "../data/dummydata/dummydata.json" 
+
+var dataNotedHouse = require('../data/dummydata/dummydata.json')
+const MAX_DISPLAY_NOTED_HOUSE = 4
+const MAX_STORED_NOTED_HOUSE = 15
+function NotedListHouse() {
+    // houseIndex is set default by useState(0 -> house index array start from 0)
+    const [houseIndex, setHouse] = useState(0);
+    const [startHouseNotedIndex, setStartHouseNotedIndex] = useState(0);
+    return (
+        generateList(dataNotedHouse)
+    )
+}
+
+function generateList(data) {
+    console.log(data.houses)
+    let listHouse = data.houses.map(houseObj => {
+        return (
+            <li key={data.houses.indexOf(houseObj)}  >
+                <Card style={{ width: "10%", height: "15%" }}>
+                    <Card.Img variant="top" src={testImg} />
+                    <Card.Body>
+                        <Card.Title>{houseObj.houseName}</Card.Title>
+                        <Card.Text>{houseObj.housePrice}</Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </li>
+        )
+    })
+    console.log(listHouse)
+    return (
+        <ul id="noted-house-list" style={{height: "80%"}}>
+            {listHouse}
+        </ul>
+    )
+}
+
+export default NotedListHouse
